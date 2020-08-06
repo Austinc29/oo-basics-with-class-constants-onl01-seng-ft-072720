1230f2d59db781df2e005e7e752581caa6fb2ed7
Austin Cummings <aac729@live.com>
2020-08-06 19:22:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,3 +12,19 @@ class Shoe
   end
 
 end
+class Shoe
+  attr_accessor :color, :size, :material, :condition, :brand
+  attr_reader :brand 
+ 
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+ 
+
+  def brand=(brand)
+    @brand = brand
+    BRAND << brand 
+  end
+end
